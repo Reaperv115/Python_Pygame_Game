c1c2a076db41d2c1ec1036bb9eaad0d15cec17f0
slaygony <rjs5785@hotmail.com>
2025-03-19 12:02:02 -0700

    have a sprite for the player drawing. now tgrying to make the background transparent

--- workspace/Main.py
+++ workspace/Main.py
@@ -3,25 +3,29 @@ import random
 
 # GLOBAL VARIABLES 
 COLOR = (255, 100, 98) 
+colorKey = (255, 255, 255)
 SURFACE_COLOR = (167, 255, 100) 
 WIDTH = 500
 HEIGHT = 500
 
-# Object class 
-class Sprite(pygame.sprite.Sprite): 
-    def __init__(self, color, height, width): 
-        super().__init__() 
-  
-        self.image = pygame.Surface([width, height]) 
-  
-        pygame.draw.rect(self.image,color,pygame.Rect(0, 0, width, height)) 
-  
-        self.rect = self.image.get_rect() 
+
+class Sprite(pygame.sprite.Sprite):
+	def __init__(self, color, width, height):
+		super().__init__()
+		self.surface = pygame.Surface([width, height])
+		self.image = pygame.image.load("space invaders player.png")
+		if self.image.get_alpha():
+			self.image.convert_alpha()
+		else:
+			self.image.convert(self.image)
+			self.surface.set_colorkey(colorKey)
+		pygame.draw.rect(self.image, color, pygame.Rect(0, 0, width, height))
+		self.rect = self.image.get_rect()
 
 
 pygame.init() 
 
-RED = (255, 0, 0) 
+white = (255, 255, 255) 
 
 size = (WIDTH, HEIGHT) 
 screen = pygame.display.set_mode(size) 
@@ -29,11 +33,11 @@ pygame.display.set_caption("Creating Sprite")
 
 all_sprites_list = pygame.sprite.Group() 
 
-objectWidth = 20
-objectHeight = 30
-object_ = Sprite(RED, objectWidth, objectHeight) 
+objectWidth = 5
+objectHeight = 10
+object_ = Sprite(white, objectWidth, objectHeight) 
 object_.rect.x = screen.get_width()/2
-object_.rect.y = screen.get_height()-objectHeight
+object_.rect.y = screen.get_height()/2
 
 all_sprites_list.add(object_) 
 
